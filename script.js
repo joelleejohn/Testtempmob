47ffa8576201ebdcff6074bbfb74449a1baa6e87
Tim John <Tim.John@LighthouseSystems.com>
2019-07-14 23:38:27 +0100

    more mobile menu styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var mobileNavigationElement = document.getElementById("menu")
-var mainElement = document.getElementsByTagName("main")[0];
+const mobileNavigationElement = document.getElementById("menu")
+const mainElement = document.getElementsByTagName("main")[0];
+const mobileNavigationChildren = mobileNavigationElement.children;
+const mobileNavigationLength = mobileNavigationChildren.length;
 
 function toggleClass(element, classToToggle){
     element.classList.toggle(classToToggle);
@@ -9,12 +11,21 @@ function setZIndex(element, zIndex){
     element.style.zIndex = zIndex;
 }
 
+function delayBackground(backgroundStyle){
+    setTimeout(() => {
+        for (let increment = 0; increment < mobileNavigationLength; increment++){
+            mobileNavigationChildren[increment].style.backgroundColor = backgroundStyle;
+        }
+    }, 400);
+}
 var toggleNavigationMenu = () => {
     toggleClass(mainElement, "menu-transform-body");
     if (mainElement.classList.contains("menu-transform-body")){
         setZIndex(mobileNavigationElement, "10");
+        delayBackground("var(--primaryColorLighter)");
     }
     else {
         setZIndex(mobileNavigationElement, "-10");
+        delayBackground("");
     }
 };
